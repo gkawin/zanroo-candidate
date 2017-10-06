d169781ec3ccbdbe542b4642d96c64d8391080e0
Kawin Glomjai <g.kawin@live.com>
2017-10-06 12:25:33 +0700

    clean up

--- src/entry.js
+++ src/entry.js
@@ -5,6 +5,7 @@ injectGlobal`
   body {
     font-family: 'Roboto', sans-serif;
   }
+  // HACK: resolve for work together with react-select and react-table.
   .ReactTable .rt-tbody .rt-td {
     overflow: visible;
   }
